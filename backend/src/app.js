66eff4e499064d7ac83a502034ad0e4055d5c36a
Lavish <lavishgarg1199@gmail.com>
2024-09-03 22:54:08 +0530

    removed / from the frontend url

--- backend/src/app.js
+++ backend/src/app.js
@@ -14,7 +14,7 @@ app.use(cookieParser());
 app.use(morgan("dev"));
 
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "https://videocave.vercel.app/");
+  res.setHeader("Access-Control-Allow-Origin", "https://videocave.vercel.app");
   res.setHeader("Access-Control-Allow-Credentials", "true");
   next();
 });
